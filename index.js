96532db4313dce68ff26ba7229014acabbe40672
sampathsris <sampathsris@gmail.com>
2015-01-05 10:23:32 +0530

    Added code to do nothing when disabled items are selected

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var EventEmitter = require('events').EventEmitter;
+var inherits = require('inherits');
 
 module.exports = function (opts) {
   return new Injector(opts || {});
@@ -8,6 +10,8 @@ function Injector(opts) {
   self.fgi = opts.fgi || 'cyan';
   self.disabled = opts.disabled || [];
 }
+
+inherits(Injector, EventEmitter);
 
 Injector.prototype.injectTo = function (tm) {
   var self = this;
@@ -26,17 +30,30 @@ Injector.prototype.injectTo = function (tm) {
     // line to confirm this). However, the foreground color
     // does not get set.
     //tm.items[index].label = '<Updated Label>';
+    
     var oldFgColor = tm.colors.fg;
     tm.colors.fg = self.fgi;
     tm._drawRow(index);
     tm.colors.fg = oldFgColor;
     
-    // TODO: save the onselect callback for future use. Replace
-    // it with a function and route execution if the selected
-    // item is not a disabled item.
   }
   
-  this.disabled.forEach(function (element) {
+  
+  // save the onselect callback for future use. Replace
+  // it with a function and route execution if the selected
+  // item is not a disabled item.
+  var l = tm.listeners('select');
+  tm.removeAllListeners('select');
+  l.forEach(function (e) {
+    self.on('selectmirror', e);
+  });
+  tm.on('select', function (label, selected) {
+    if (self.disabled.indexOf(label) == -1) {
+      self.emit('selectmirror', label, selected);
+    }
+  });
+  
+  self.disabled.forEach(function (element) {
     tm.disable(element);
   });
 }
